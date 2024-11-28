18a6935bc85dd3bb7c0b7ddc360f40e239c31748
Otthon Leão <otthonleao@hotmail.com>
2024-11-27 20:02:47 -0400

    chore: start user-services-api project

---
+++ user-services-api/src/test/java/dev/otthon/helpdesk/userservicesapi/UserServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.helpdesk.userservicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
