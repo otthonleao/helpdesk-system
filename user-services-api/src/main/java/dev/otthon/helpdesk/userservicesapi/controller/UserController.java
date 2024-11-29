661e78e7b2f4207e162375eaebd33a6890ad9b3f
Otthon Leão <otthonleao@hotmail.com>
2024-11-29 19:21:25 -0400

    chore: add springdoc-openapi dependency, config and swagger-doc in POST method

--- user-services-api/src/main/java/dev/otthon/helpdesk/userservicesapi/controller/UserController.java
+++ user-services-api/src/main/java/dev/otthon/helpdesk/userservicesapi/controller/UserController.java
@@ -1,10 +1,19 @@
 package dev.otthon.helpdesk.userservicesapi.controller;
 
 import dev.otthon.helpdesk.userservicesapi.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import model.exceptions.UserAlreadyExistsException;
 import model.request.CreateUserRequest;
 import model.response.UserResponse;
 
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,17 +23,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "UserController", description = "Controller for user operations")
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserAlreadyExistsException.class)))
+    })
     @PostMapping
-    public ResponseEntity<UserResponse> create(@RequestBody final CreateUserRequest createUserRequest) {
+    public ResponseEntity<UserResponse> create(
+            @Parameter(description = "User id", required = true, example = "674808b02ff2181e545a8778")
+            @RequestBody final CreateUserRequest createUserRequest) {
+
         UserResponse createdUser = userService.insert(createUserRequest);
-        // Para gerar URI dinâmicas e mandar no header da resposta
+
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
