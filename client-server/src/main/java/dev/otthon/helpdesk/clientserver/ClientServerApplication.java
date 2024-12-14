651debf0ed41e33c02fc256ca3a1f8a72b2e321b
Otthon Leão <otthonleao@hotmail.com>
2024-12-14 03:04:45 -0400

    chore: start config server project

---
+++ client-server/src/main/java/dev/otthon/helpdesk/clientserver/ClientServerApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.helpdesk.clientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerApplication.class, args);
+    }
+
+}
