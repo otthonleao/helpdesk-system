a30a63d9cc05004fde152a18d6afc7dddee9981d
Otthon Leão <otthonleao@hotmail.com>
2024-12-15 01:22:41 -0400

    chore: started project

---
+++ order-service-api/src/test/java/dev/otthon/helpdesk/orderservice/OrderServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.helpdesk.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
