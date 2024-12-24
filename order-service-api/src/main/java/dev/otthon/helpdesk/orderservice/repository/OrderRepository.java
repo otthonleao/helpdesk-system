6273973289bc6261a87ada31a44e6ea6b0079305
Otthon Leão <otthonleao@hotmail.com>
2024-12-24 01:52:56 -0400

    fix: change class name

--- order-service-api/src/main/java/dev/otthon/helpdesk/orderservice/repository/OrderRepository.java
+++ order-service-api/src/main/java/dev/otthon/helpdesk/orderservice/repository/OrderRepository.java
@@ -3,6 +3,6 @@ package dev.otthon.helpdesk.orderservice.repository;
 import dev.otthon.helpdesk.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
