6fc50b4f9f7ded97f3d05825bd059e841a5b1ee0
Otthon Leão <otthonleao@hotmail.com>
2024-12-27 21:46:11 -0400

    feat: add endpoint GET to show by ID

--- order-service-api/src/main/java/dev/otthon/helpdesk/orderservice/controller/OrderController.java
+++ order-service-api/src/main/java/dev/otthon/helpdesk/orderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package dev.otthon.helpdesk.orderservice.controller;
 
+import dev.otthon.helpdesk.orderservice.mapper.OrderMapper;
 import dev.otthon.helpdesk.orderservice.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import model.request.CreateOrderRequest;
 import model.request.UpdateOrderRequest;
@@ -27,6 +29,23 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class OrderController {
 
     private final OrderService service;
+    private final OrderMapper mapper;
+
+    @Operation(summary = "Find order by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order found"),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> findById(
+            @NotNull(message = "The order id must be informed")
+            @Parameter(description = "Order ID", example = "2", required = true)
+            @PathVariable final Long id
+    ) {
+        return ResponseEntity.ok().body(mapper.fromEntity(service.findById(id)));
+    }
 
     @Operation(summary = "Update order")
     @ApiResponses(value = {
